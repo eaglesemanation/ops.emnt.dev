a85b8a734973b92537c8d20451361120343ed82d
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-05-09 05:07:54 -0400

    fix(mailserver): Fix fail on container restart

--- k8s/apps/network/mailserver/wireguard-entrypoint.sh
+++ k8s/apps/network/mailserver/wireguard-entrypoint.sh
@@ -6,30 +6,30 @@ if [ -z "$GATEWAY_IP" ]; then echo "GATEWAY_IP env var is required"; exit 255; f
 if [ ! -f /etc/os-release ]; then echo "Could not identify distribution"; exit 255; fi
 if [ ! -f /etc/wireguard/wg0.conf ]; then echo "/etc/wireguard/wg0.conf is required"; exit 255; fi
 
-. /etc/os-release
-
-case $ID in
-    alpine)
-        apk update
-        apk add wireguard-tools curl
-        ;;
-    *)
-        echo "This distribution is not supported"
-        exit 255
-        ;;
-esac
-
 if ip addr | grep -q wg0; then
     ip link del wg0
+else
+    . /etc/os-release
+
+    case $ID in
+        alpine)
+            apk update
+            apk add wireguard-tools curl
+            ;;
+        *)
+            echo "This distribution is not supported"
+            exit 255
+            ;;
+    esac
+
+    K8S_GW_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+
+    # command might fail if rule already set
+    ip route add "$GATEWAY_IP" via "$K8S_GW_IP" || /bin/true
+    for local_cidr in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/24"; do
+        ip route add "$local_cidr" via "$K8S_GW_IP" || /bin/true
+    done
 fi
-
-K8S_GW_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
-
-# command might fail if rule already set
-ip route add "$GATEWAY_IP" via "$K8S_GW_IP" || /bin/true
-for local_cidr in "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/24"; do
-    ip route add "$local_cidr" via "$K8S_GW_IP" || /bin/true
-done
 
 echo "Deleting existing default GWs"
 ip route del 0/0 || /bin/true
