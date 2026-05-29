d36a8936c5f89f28dff192fb507b57b6c7f4051d
Vladimir Romashchenko <eaglesemanation@gmail.com>
2026-05-29 16:42:25 -0400

    fix(chisel-server): Use LoadCredential

--- nix/chisel.nix
+++ nix/chisel.nix
@@ -8,13 +8,32 @@
     pkgs,
     config,
     ...
-  }: {
+  }: let
+    cfg = config.services.chisel-server;
+  in {
     sops.secrets."chisel/authfile.json" = {};
     services.chisel-server = {
       enable = true;
       reverse = true;
       port = 9090;
-      authfile = config.sops.secrets."chisel/authfile.json".path;
+      host = "0.0.0.0";
+    };
+    networking.firewall.allowedTCPPorts = [9090];
+    systemd.services.chisel-server.serviceConfig = {
+      LoadCredential = "authfile:${config.sops.secrets."chisel/authfile.json".path}";
+      # Overriding due to cfg.authfile being restricted to path type
+      ExecStart = [
+        ""
+        ("${pkgs.chisel}/bin/chisel server --authfile %d/authfile "
+          + lib.concatStringsSep " " (
+            lib.optional (cfg.host != null) "--host ${cfg.host}"
+            ++ lib.optional (cfg.port != null) "--port ${toString cfg.port}"
+            ++ lib.optional (cfg.keepalive != null) "--keepalive ${cfg.keepalive}"
+            ++ lib.optional (cfg.backend != null) "--backend ${cfg.backend}"
+            ++ lib.optional cfg.socks5 "--socks5"
+            ++ lib.optional cfg.reverse "--reverse"
+          ))
+      ];
     };
   };
 }
